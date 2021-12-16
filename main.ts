e29f98c9592eb9dff13dd22b81cda8b0fe343611
Jeff <jeff@cusk.io>
2021-12-16 14:56:58 -0700

    use pattern defined in a variable instead of fixed pattern

--- main.ts
+++ main.ts
@@ -1,9 +1,37 @@
-let counter = 0
+let pos = 0
+let index = 0
+let xy_pattern = [
+22,
+23,
+13,
+12,
+11,
+21,
+31,
+32,
+33,
+34,
+24,
+14,
+4,
+3,
+2,
+1,
+0,
+10,
+20,
+30,
+40,
+41,
+42,
+43,
+44
+]
 loops.everyInterval(1000, function () {
-    led.toggle(counter / 5, counter % 5)
-    if (counter == 24) {
-        counter = 0
-    } else {
-        counter += 1
+    pos = xy_pattern[index]
+    led.toggle(pos / 10, pos % 10)
+    index += 1
+    if (index == xy_pattern.length) {
+        index = 0
     }
 })
